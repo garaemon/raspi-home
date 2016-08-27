447ca095d66a22d7e5740639e791b546bfcf6ded
Ryohei Ueda <garaemon@gmail.com>
2016-08-28 03:26:50 +0900

    Fix sentence to speak when booted (#15)

--- python/bin/run_raspi_server.py
+++ python/bin/run_raspi_server.py
@@ -7,16 +7,17 @@ import os
 from raspi_server.bot import Bot
 from raspi_server.say import say
 
+
 def main():
+    "main function"
     bot = Bot()
     bot.run()
     bot.send_message_to("iot_channel", "booted")
-    say("サーバが立ち上がりました")
+    say("サーバーを立ち上げました")
     bot.loop()
-    
+
 if __name__ == '__main__':
     # check os.environ
     if not os.environ.has_key("SLACKBOT_API_TOKEN"):
         raise Exception("You should specify SLACKBOT_API_TOKEN")
     main()
-    
