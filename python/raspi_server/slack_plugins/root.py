44ad194b4d297b99789a19039abd463e5f224c3e
Ryohei Ueda <garaemon@gmail.com>
2016-08-27 23:49:21 +0900

    Remove weather.py and add root.py to collect all the subcommands (#10)

--- python/raspi_server/slack_plugins/root.py
+++ python/raspi_server/slack_plugins/root.py
@@ -6,7 +6,10 @@ import re
 from raspi_server.say import say
 
 @listen_to('.*', re.IGNORECASE)
-def speach_weather(message):
-    if message.channel._body['name'] == 'iot_weather':
+def all_message_dispatch(message):
+    if message.channel._body['name'] == 'iot_speach':
+        say(message._body['text'])
+    elif message.channel._body['name'] == 'iot_weather':
         say("雨が振りそうです。洗濯物をたたんでください")
         message.reply("Spoken")
+
