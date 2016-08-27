bb18cb9f4e9fa967deab15e8a04a83d3ccc169fa
Ryohei Ueda <garaemon@gmail.com>
2016-08-28 03:02:25 +0900

    Fix say function by utf-8 handling (#12)
    * slack message is unicode string
    * encode that unicode string with encode('utf-8') method and pass it to
      open_jtalk process.

--- python/raspi_server/slack_plugins/root.py
+++ python/raspi_server/slack_plugins/root.py
@@ -12,7 +12,7 @@ from raspi_server.say import say
 def all_message_dispatch(message):
     "dispatch according to any received messages"
     if message.channel._body['name'] == 'iot_speach':
-        say(message._body['text'])
+        say(message._body['text'].encode('utf-8'))
     elif message.channel._body['name'] == 'iot_weather':
         say("雨が振りそうです。洗濯物をたたんでください")
         message.reply("Spoken")
