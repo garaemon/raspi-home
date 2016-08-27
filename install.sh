9982fee0aaa0c7be80523636dfe4f1ff9ffb1525
Ryohei Ueda <garaemon@gmail.com>
2016-08-27 12:12:07 +0900

    Add script to setup virtualenv (#4)
    * Update python to 2.7.10
    * Add script to setup virtualenv
    * Test install.sh
    * Syntax check of bash script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+PYTHON_VERSION=2.7.10
+
+# source pyenv
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
+export PYTHON_ROOT=$(pyenv prefix)
+
+if ! pyenv versions | grep $PYTHON_VERSION; then
+    pyenv install $PYTHON_VERSION
+fi
+
+# setup virtualenv
+if [ ! -e raspi-server/env ] ; then
+    (cd raspi-server &&
+     pyenv local $PYTHON_VERSION &&
+     virtualenv env)
+fi
+
+cd raspi-server
+
+source env/bin/activate
+pip install -r requirements.txt
