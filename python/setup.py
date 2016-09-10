e18270c25e66fcb186ed5412535e28002519139d
Ryohei Ueda <garaemon@gmail.com>
2016-09-11 02:42:52 +0900

    Add raspi_server.utils

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
     name='raspi_server',
     version='0.0',
-    packages=['raspi_server', 'raspi_server.slack_plugins', 'raspi_server.tasks'],
+    packages=['raspi_server', 'raspi_server.slack_plugins',
+              'raspi_server.tasks', 'raspi_server.utils'],
     scripts=['bin/run_raspi_server.py']
     )
