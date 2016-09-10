6b579b66a0ac20a752551f78d4e563f886e8ac2e
Ryohei Ueda <garaemon@gmail.com>
2016-09-11 02:28:04 +0900

    Fix syntax

--- python/raspi_server/utils/exceptions.py
+++ python/raspi_server/utils/exceptions.py
@@ -4,9 +4,11 @@
 Provide exception classes used in raspi-home project.
 """
 
+
 class RaspiHomeException(Exception):
     "Root exception class used in raspi-home project"
     pass
+
 
 class MethodNotImplemented(RaspiHomeException):
     "Exception which is raised when unimplemented method is called"
