325ce43b87c1274ddc1dd164ee6e58819d214e13
Ryohei Ueda <garaemon@gmail.com>
2016-09-11 02:41:47 +0900

    Add raspi_server.tasks module to setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 setup(
     name='raspi_server',
     version='0.0',
-    packages=['raspi_server', 'raspi_server.slack_plugins'],
+    packages=['raspi_server', 'raspi_server.slack_plugins', 'raspi_server.tasks'],
     scripts=['bin/run_raspi_server.py']
     )
