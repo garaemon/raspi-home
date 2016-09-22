4b38d66f81031544949ac6d2a148097dfff29079
Ryohei Ueda <garaemon@gmail.com>
2016-09-22 17:44:33 +0900

    Fix sentence to say when raining

--- python/raspi_server/tasks/weather.py
+++ python/raspi_server/tasks/weather.py
@@ -15,5 +15,5 @@ class WeatherTask(Task):
         return channel == "iot_weather"
 
     def invoke(self, channel, text, message):
-        say("雨が振りそうです。洗濯物をたたんでください")
+        say("雨が振りそうです。洗濯物をとりこんで")
         message.reply("Spoken")
